ecf3a35987916397a4efb5e4ed63a2269dcaa18c
Sean OReilly <soreilly424@gmail.com>
2021-01-16 11:01:15 -0500

    lazy loading with oldrer react

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,12 +2,22 @@ import React, { Component } from "react";
 // import axios from "axios";
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
+
 import "./Blog.css";
-import Posts from './Posts/Posts'
-import NewPost from './NewPost/NewPost'
+import Posts from './Posts/Posts';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent'
+
+
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost')
+})
+
 
 class Blog extends Component {
-  
+  state = {
+    auth: true
+  }
   render() {
 
     return (
@@ -32,10 +42,11 @@ class Blog extends Component {
           </nav>
         </header>
         <Switch>
-          <Route path="/new-post" component={NewPost} />
+          {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null }
           <Route path="/posts" component={Posts} />
+          <Route render={() => <h1>Not found</h1>} />
           {/* <Route path="/" component={Posts} /> */}
-          <Redirect from="/" to="/posts" />
+          {/* <Redirect from="/" to="/posts" /> */}
         </Switch>
       </div>
     );
